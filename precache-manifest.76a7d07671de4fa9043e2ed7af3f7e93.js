88d62754b728342a9bc61a97302d4eaa7e774e0e
Patrick ONeill <poneill@sembracare.com>
2020-01-08 19:40:56 -0500

    Updates

---
+++ precache-manifest.76a7d07671de4fa9043e2ed7af3f7e93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87e08d41e73827156af320b550b146c",
+    "url": "/memoryGame/index.html"
+  },
+  {
+    "revision": "41870464883f4b7e43e8",
+    "url": "/memoryGame/static/js/2.45d50d2f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memoryGame/static/js/2.45d50d2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39aab146eb49f305dc50",
+    "url": "/memoryGame/static/js/main.0183a5d2.chunk.js"
+  },
+  {
+    "revision": "f2ac2f6990a4f1de5ef8",
+    "url": "/memoryGame/static/js/runtime-main.d96aa32c.js"
+  },
+  {
+    "revision": "c87090fd3fd924b53335ed4e1c674fc1",
+    "url": "/memoryGame/static/media/image1.c87090fd.jpg"
+  },
+  {
+    "revision": "4818fe8e00f08aadffe8bdcc3c07f5c0",
+    "url": "/memoryGame/static/media/image12.4818fe8e.jpg"
+  },
+  {
+    "revision": "d5335fdb5307a8b432863fcb9a974a73",
+    "url": "/memoryGame/static/media/image3.d5335fdb.jpg"
+  }
+]);
